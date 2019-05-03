0db58142c3b4100160d1cd5a03c54c5a0b6a74ce
hugolevino <50059470+hugolevino@users.noreply.github.com>
2019-05-03 00:01:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const {BigQuery} = require('@google-cloud/bigquery');
+const cloudTasks = require('@google-cloud/tasks');
+const client = new cloudTasks.CloudTasksClient();
+const parent = client.queuePath('bigdata-bernard', 'us-central1', 'first-queue');
+const bodyParser = require('body-parser');
 const express = require('express');
 const rp = require('request-promise');
-const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,9 +13,173 @@ app.use(bodyParser.raw());
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
+const task = {
+	appEngineHttpRequest: {
+    	httpMethod: 'POST',
+    	relativeUri: '/listening',
+	},
+};
+
+var rows = '';
+var known_list = new Array();
+var final_list = new Array();
+var count_seller = 0;
+var count_row = 0;
+var im_first = 'n';
+
+app.get('/start2', (req, res) => {
+ 	
+	query();
+	res.status(200);
+	res.send('INICIOU');
+	res.end();
+
+});
+
+async function query_inicial() {
+
+	rows = '';
+	known_list = new Array();
+	final_list = new Array();
+	count_seller = 0;
+	count_row = 0;
+	im_first = 'n';
+
+	const bigqueryClient = new BigQuery();
+
+	const query = `SELECT cnpj FROM \`bigdata-bernard.my_new_dataset.data_ativacao\``;
+	const options = {
+		query: query,
+		location: 'US',
+	};
+
+	const [job] = await bigqueryClient.createQueryJob(options);
+	console.log(`Job ${job.id} started.`);
+
+	rows = await job.getQueryResults();
+
+	for (i = 0; i < rows[0].length; i++) {
+		known_list.push(rows[0][i].cnpj);
+	}
+
+	get_cats();
+  
+}
+
+
+function get_cats(){
+	
+	var options = {
+		method: 'GET',
+		resolveWithFullResponse: true,
+		uri: 'https://turbo-v1-americanas.b2w.io/slug/sitemap/seller?limit=100000',
+		headers: {
+			"content-type": "application/json",
+			"accept": "application/json",
+		},
+		json: true
+	};
+
+	rp(options).then(function (repos) {
+		
+
+		for (i = 0; i < repos.body.itens.length; i++) {
+			cnpj_to_int = repos.body.itens[i].id;
+			if (known_list.indexOf(parseInt(cnpj_to_int)) == -1) {
+				if (final_list.indexOf(parseInt(cnpj_to_int)) == -1) {
+					var teste = parseInt(cnpj_to_int);
+					var teste = teste.toString();
+					final_list.push(teste);
+					count_seller++;
+					console.log(count_seller + ' -> ' + cnpj_to_int);
+				}
+			}
+		}
+
+		queueing();
+
+	})
+	.catch(function (err) {
+		
+		console.log(err);
+
+	});
+	
+}
+
+async function queueing(){
+
+	if(count_row < final_list.length){
+		for (i = 0; i < 10; i++) {
+			if(count_row < final_list.length){
+				
+				var payload = final_list[count_row];
+				task.appEngineHttpRequest.body = Buffer.from(payload).toString('base64');
+				const request = {
+				    parent: parent,
+				    task: task,
+				};
+
+				try {
+					const [response] = await client.createTask(request);
+					console.log(count_row + ' --> QUEUED');
+		    	} catch(e) {
+					return;
+					console.log(count_row + ' --> ERRO QUEUED');
+
+		    	}
+
+				 
+				count_row++;
+			}else{
+				if(im_first == 'n'){
+					im_first = 's';
+					setTimeout(function(){
+						query_inicial();
+					}, 15000);
+				}	
+			}
+		}
+		setTimeout(function(){
+			 queueing();
+		}, 5000);
+	}else{
+		if(im_first == 'n'){
+			im_first = 's';
+			setTimeout(function(){
+				query_inicial();
+			}, 15000);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.post('/listening', (req, res) => {
  	
- 	//var cnpj = '23450889000104';
  	var cnpj = req.body;
 
 	var options = {
